47362d61b9d3c777903c83a5aa379376629e6730
VPazenko <vpazenko1994@gmail.com>
2025-03-25 16:48:54 +0100

    add exploration function

--- dask/module_dask.py
+++ dask/module_dask.py
@@ -76,6 +76,44 @@ def initial_preprocessing_dask(log, df_clin):
     return df_clin
 
 
+def data_exploration_dask(log, df_clin, df_exp):
+    # Start timer
+    start = time.time()
+    
+    df_exp = df_exp.set_index('!Sample_title')
 
+    # Вычисляем стандартное отклонение всех генов
+    std_series = df_exp.std(axis=1).compute()
+
+    # Получаем топ-10 наиболее вариативных генов
+    top_genes = std_series.nlargest(10).index
+    df_selected = df_exp.loc[top_genes.to_list(),:]
+
+    # Транспонируем результат (если данных не очень много)
+    df_transposed = df_selected.compute().T  # Транспонируем в Pandas
+    df_transposed = dd.from_pandas(df_transposed, npartitions=4)  # Конвертируем обратно в Dask
+
+    df_clin = df_clin.set_index('title')
+    df_combined = df_clin.merge(df_transposed, how='left', left_index=True, right_index=True)
+    
+    # Create TumorSubtype column
+    df_combined['TumorSubtype'] = df_combined['characteristics.tag.histology'].str.contains("Squamous", case=False, na=False).astype(int)
+    df_combined = df_combined.drop(columns=['characteristics.tag.histology'], errors='ignore')
+    
+    # Reduce number of primary stage categories
+    df_combined['characteristics.tag.stage.primary.tumor'] = df_combined['characteristics.tag.stage.primary.tumor'].str[:3]
+    
+    # Compute grouped mean values
+    last_10_columns = df_combined.columns[-11:-1]
+    grouped_df = df_combined.groupby("TumorSubtype")[last_10_columns].mean().reset_index()
+    
+    # Plot distribution with reduced memory usage
+    plot_values_distribution(df_combined.loc[:,["TumorSubtype"]].compute(), "TumorSubtype", name='dask')
+    
+    # End timer
+    exploration_time = time.time() - start
+    log.info(f"3. Dask data exploration time: {exploration_time:.2f} seconds")
+    
+    return df_combined, grouped_df
 
 
