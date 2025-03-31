5ff00885bd124081b04803a60f19c9a45bb97625
VPazenko <vpazenko1994@gmail.com>
2025-03-31 21:11:51 +0200

    Add docstrings, correct text to log

--- dask/module.py
+++ dask/module.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+'''
+These module provide functionality for main preproceessing of the data and modelling xgboost with pandas library.
+'''
+
 __author__ = "V.Pazenko"
 __version__ = 1.0
 
@@ -13,6 +17,16 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc
 
 
 def load_data_pd(log, metadata='data/Lung3.metadata.xlsx', expression_data="data/GSE58661_series_matrix.txt.gz"):
+    '''
+    Input: 1. log = loger
+           2. metadata = path to the metadata file
+           3. expression_data = path to the expression data file
+
+    Function loads these 2 files into Pandas DataFrame and calculate time and memory usage.
+
+    Output: 1. df_clin = pd.DataFrame with clinical data
+            2. df_exp = pd.DataFrame with expression data
+    '''
     # Start timer
     start = time.time()
     # Load metadata
@@ -34,6 +48,14 @@ def load_data_pd(log, metadata='data/Lung3.metadata.xlsx', expression_data="data
 
 
 def initial_preprocessing_pd(log, df_clin):
+    '''
+    Input: 1. log = loger
+           2. df_clin = pd.DataFrame with clinical data
+
+    Function preprocess the data. Remove columns with only one unique value, remove duplicates, fill in missing values.
+
+    Output: 1. df_clin = processed pd.DataFrame with clinical data
+    '''
     # Start timer
     start = time.time()
     # Remove columns with only one unique value
@@ -61,6 +83,18 @@ def initial_preprocessing_pd(log, df_clin):
 
 
 def data_exploration_pd(log, df_clin, df_exp):
+    '''
+    Input: 1. log = loger
+           2. df_clin = pd.DataFrame.DataFrame with clinical data
+           3. df_exp = pd.DataFrame with expression data
+
+    Function select top 10 genes with the highest standard deviation and merge them with clinical data.
+    Function also create a new column "TumorSubtype" (y for ML) based on the "characteristics.tag.histology" column.
+    Also create a distribution boxplot for the "TumorSubtype" column.
+
+    Output: 1. df_combined = pd.DataFrame with clinical data and top 10 genes
+            2. grouped_df = pd.DataFrame with the average expression of selected genes for each subtype
+    '''
     # Start timer
     start = time.time()
     df_exp = df_exp.set_index('!Sample_title').T
@@ -90,6 +124,16 @@ def data_exploration_pd(log, df_clin, df_exp):
 
 
 def plot_values_distribution(df, column, name='pandas'):
+    '''
+    Input: 1. df = Dataframe that have to have at least clumn [column] (for Dask it should be df.compute())
+           2. column = name of the column of interest
+           3. name = name of module (dask or pandas)
+
+    Function will plot the distribution of the values in the column [column] and save it to the "plots" folder.
+    Universal for Dask and Pandas, but for Dask it should be df.compute() before using this function.
+
+    Output: save plot.png to the "plots" folder
+    '''
     plt.figure(figsize=(6, 4))
     if not os.path.exists("plots"):
         os.makedirs("plots")
@@ -104,33 +148,51 @@ def plot_values_distribution(df, column, name='pandas'):
 
 
 def preprocessing_train_test_pd(log, df_combined):
+    '''
+    Input: 1. log = loger
+           2. df_combined = pd.DataFrame with clinical data and top 10 genes
+
+    Function will preprocess the data for train/test split and make this split.
+
+    Output: X_train, X_test, y_train, y_test standard train/test split result
+    '''
     # Start timer
     start = time.time()
 
     X = df_combined.iloc[:,:-1]
     cat_columns = df_combined.iloc[:,:-1].select_dtypes(include=['object']).columns
     num_columns = df_combined.iloc[:,:-1].select_dtypes(include=['number']).columns
+    # Convert categorical columns
     X = pd.get_dummies(X, columns = cat_columns, drop_first=True)
     y = df_combined.iloc[:,-1]
+    # Split the data with stratification
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
 
     # Do not leak data from test to train
     mean = X_train[num_columns].mean()
     std = X_train[num_columns].std()
 
-    # use z-score
+    # use z-score scaler
     X_train[num_columns] = (X_train[num_columns] - mean) / std
     X_test[num_columns] = (X_test[num_columns] - mean) / std
 
     # End timer
     preprocess_time = time.time() - start
 
-    log.info(f"4. Pandas preprocessing time: {preprocess_time:.2f} seconds")
+    log.info(f"4. Pandas preprocessing (train/test) time: {preprocess_time:.2f} seconds")
 
     return X_train, X_test, y_train, y_test
 
 
 def modelling_XGBoost_pd(log, df_combined):
+    '''
+    Input: 1. log = loger
+           2. df_combined = pd.DataFrame with clinical data and top 10 genes
+
+    Function will train and implement XGBoost model on the data.
+
+    Output: model metrics, roc_curve.png
+    '''
     X_train, X_test, y_train, y_test = preprocessing_train_test_pd(log, df_combined)
     # start timer
     start_time = time.time()
@@ -156,16 +218,27 @@ def modelling_XGBoost_pd(log, df_combined):
     model_time = time.time() - start_time
     log.info(f"5. Pandas modelling time: {model_time:.2f} seconds")
 
-    log.info(f"Model accuracy: {accuracy_pandas:.2f}")
-    log.info(f"Model precision: {precision:.2f}")
-    log.info(f"Model recall: {recall:.2f}")
-    log.info(f"Model F1-score: {f1:.2f}")
-    log.info(f"Model AUC-ROC: {auc_roc_pandas:.2f}")
+    log.info(f"5. Pandas Model accuracy: {accuracy_pandas:.2f}")
+    log.info(f"5. Pandas Model precision: {precision:.2f}")
+    log.info(f"5. Pandas Model recall: {recall:.2f}")
+    log.info(f"5. Pandas Model F1-score: {f1:.2f}")
+    log.info(f"5. Pandas Model AUC-ROC: {auc_roc_pandas:.2f}")
 
     plot_roc_curve(y_test, y_pred_pandas, auc_roc_pandas, name='pandas')
 
 
 def plot_roc_curve(y_test, y_pred, auc_score, name='pandas'):
+    '''
+    Input: 1. y_test = real values of the target variable
+           2. y_pred = predicted values of the target variable
+           3. auc_score = calculated AUC score
+           4. name = name of module (dask or pandas)
+
+    Function will plot the ROC curve and save it to the "plots" folder.
+    Universal for Dask and Pandas.
+
+    Output: save plot.png to the "plots" folder
+    '''
     plt.figure(figsize=(8, 6))
     fpr, tpr, _ = roc_curve(y_test, y_pred)
     plt.plot(fpr, tpr, label=f"AUC = {auc_score:.3f}")
